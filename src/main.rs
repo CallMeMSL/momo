de857b5efdee4013a25279c3428d5066478f7e8e
CallMeMSL <david-schwab@gmx.de>
2025-02-12 20:09:00 +0100

    remove seaorm

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::fs::File;
 use std::io::Read;
 use anyhow::Result;
 
-mod db;
 pub mod torrent;
 
 fn main() -> Result<()> {
--- update-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-sea-orm-cli migrate up
-sea-orm-cli generate entity -o entity/src -l
